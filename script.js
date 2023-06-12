1bff8a69bf321135d25f22cccb658f1e2c877af1
RazelRaz <razel16dcm@gmail.com>
2023-06-12 23:33:57 +0600

    nav menu done

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// nav menu toggle scripts for small devices
 var hamOpen = document.querySelector('.ham_open');
 var dNone = document.querySelector('.d_none');
+var smMenu = document.querySelector('.nav__area-sm-menu');
+var smMenu_content_link = document.querySelector('.nav__area-sm-menu-content_links');
 
 hamOpen.addEventListener('click', function() {
   toggleVisibility();
@@ -12,4 +15,9 @@ dNone.addEventListener('click', function() {
 function toggleVisibility() {
   hamOpen.classList.toggle('d_none');
   dNone.classList.toggle('d_none');
+  smMenu.classList.toggle('sm__menu-open');
 }
+
+smMenu_content_link.addEventListener('click', function() {
+    smMenu.classList.remove('sm__menu-open');
+})
